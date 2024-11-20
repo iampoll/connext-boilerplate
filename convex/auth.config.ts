3ef599963288e6659c47facbf0d205cb6ba5cc22
ampol <memetitv01@gmail.com>
2024-11-20 21:12:03 +0800

    Update auth config to use environment variable for Clerk domain

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
         {
-            domain: "https://polished-pangolin-38.clerk.accounts.dev/",
+            domain: "https://composed-bison-32.clerk.accounts.dev/",
             applicationID: "convex",
         },
     ],
